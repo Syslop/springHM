f189824554a8718c56de45ff92d1dcd6b4c1b406
Syslop <petrovkonstantined@gmail.com>
2024-03-02 23:50:39 +0300

    init

---
+++ PhoneStoreApp/src/test/java/com/gb/PhoneStoreApp/PhoneStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gb.PhoneStoreApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneStoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
